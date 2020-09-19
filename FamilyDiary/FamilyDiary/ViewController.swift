98d1dc1c5a5cd9de48238d80d1ec178eddd79fd7
limdan7 <limdan7@berkeley.edu>
2020-09-19 14:12:20 +0900

    XCode App 공용
    첫 업뎃

---
+++ FamilyDiary/FamilyDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FamilyDiary
+//
+//  Created by 임다현 on 2020/09/19.
+//  Copyright © 2020 Family Diary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
