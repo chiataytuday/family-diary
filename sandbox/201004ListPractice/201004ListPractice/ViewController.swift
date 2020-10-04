5844600e72caab62d600c52b0a146182c7c7f08f
limdan7 <limdan7@berkeley.edu>
2020-10-04 10:58:45 +0900

    sandbox 업데이트
    업데이트

---
+++ sandbox/201004ListPractice/201004ListPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  201004ListPractice
+//
+//  Created by Daniel Lim on 2020/10/04.
+//  Copyright © 2020 Daniel Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
