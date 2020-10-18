f4a8d7504d7c9213e108d4c173b0af8a3e5bbd1a
김초희 <qwerpoiu1316@naver.com>
2020-10-18 12:51:13 +0900

    User 모델 작성

--- FamilyDiary/FamilyDiary/Model/User.swift
+++ FamilyDiary/FamilyDiary/Model/User.swift
@@ -10,5 +10,14 @@ import Foundation
 
 struct User {
     let userId: Int
+    let familyId: Int
+    var name: String
 }
 
+var dummy_users: [User] = [
+    User(userId: 0, familyId: 0, name: "엄마"),
+    User(userId: 1, familyId: 0, name: "아빠"),
+    User(userId: 2, familyId: 0, name: "다현"),
+    User(userId: 3, familyId: 0, name: "성태"),
+    User(userId: 4, familyId: 0, name: "초희"),
+]
