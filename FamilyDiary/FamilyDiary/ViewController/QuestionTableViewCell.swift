59065ccc0f4d7216508978a2e39c8e0fe5b1c957
limdan7 <limdan7@berkeley.edu>
2020-10-25 14:19:27 +0900

    UI 퀘스쳔 리스트 업데이트
    푸쉬합니다.

---
+++ FamilyDiary/FamilyDiary/ViewController/QuestionTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  QuestionTableViewCell.swift
+//  FamilyDiary
+//
+//  Created by Daniel Lim on 2020/10/25.
+//  Copyright © 2020 Family Diary. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var questionID: UILabel!
+    @IBOutlet weak var questionDescription: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
