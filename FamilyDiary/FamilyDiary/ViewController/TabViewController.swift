02e97df0d33c318e3afb6c52a6ff4578ef428b3d
Kimchohee <chchgml10@gmail.com>
2020-11-04 23:13:40 +0900

    Add : Code 공유 및 입력하고, TabBar로 넘어가는 기능 구현

---
+++ FamilyDiary/FamilyDiary/ViewController/TabViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TabViewController.swift
+//  FamilyDiary
+//
+//  Created by 김초희 on 2020/11/04.
+//  Copyright © 2020 Family Diary. All rights reserved.
+//
+
+import UIKit
+
+class TabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
