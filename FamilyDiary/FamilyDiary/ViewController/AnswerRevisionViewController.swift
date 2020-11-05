e81467a9d6c358e1b0206d2bd6577a4d6d4abcaa
limdan7 <limdan7@berkeley.edu>
2020-11-05 09:39:31 +0900

    develop - 전체 뷰  - 기능 1차 완성

---
+++ FamilyDiary/FamilyDiary/ViewController/AnswerRevisionViewController.swift
@@ -0,0 +1,94 @@
+//
+//  AnswerRevisionViewController.swift
+//  FamilyDiary
+//
+//  Created by Daniel Lim on 2020/11/04.
+//  Copyright © 2020 Family Diary. All rights reserved.
+//
+
+import UIKit
+
+class AnswerRevisionViewController: UIViewController {
+
+    @IBOutlet weak var questionDescription: UILabel!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var wordCounterLabel: UILabel!
+    @IBOutlet weak var answeredUser: UILabel!
+    @IBOutlet weak var answerTextView: UITextView!
+    
+    @IBAction func saveButton(_ sender: Any) {
+        answerList[row!][findIndexOfAnswer()] = Answer(answeredUser: currentUser, answerDescription: answerTextView.text)
+        navigationController?.popViewController(animated: true)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    var row: Int?
+    var question: Question?
+    var answers: [Answer]?
+    var answerInput: String?
+    var answeredUsersLabel: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        saveButton.layer.cornerRadius = 25
+        saveButton.layer.borderWidth = 2.0
+        answerTextView.layer.borderWidth = 0.5
+        answerTextView.layer.cornerRadius = 10
+        answerTextView.textContainerInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        
+        
+        questionDescription.text = question!.questionDescription
+        answeredUserDescription()
+        // 해당 답변을 작성한 유저에 대해서 띄우기.
+        answerTextView.text = answers?[findIndexOfAnswer()].answerDescription
+        wordCounterLabel.text = "\(answerTextView.text.count)/200"
+        
+    }
+    
+//
+//    @IBAction func saveAnswer(_ sender: Any) {
+//        answerList[row!].append(Answer(answeredUser: currentUser, answerDescription: answerTextView.text))
+//        print(answerList)
+//        navigationController?.popViewController(animated: true)
+//        dismiss(animated: true, completion: nil)
+//    }
+    
+    func answeredUserDescription() {
+        var answeredUsersName : String = ""
+        for answer in answers!{
+            answeredUsersName += "\(answer.answeredUser.userName)"
+        }
+        if answeredUsersName == "" {
+            answeredUser.text = "아직 아무도 답변하지 않았어요."
+        } else {
+            answeredUser.text = "답변한 유저: \(answeredUsersName)"
+        }
+    }
+    
+    func findIndexOfAnswer() -> Int {
+        let index = answers?.index(where: {$0.answeredUser.userName == currentUser.userName})
+        return index!
+    }
+}
+
+extension AnswerRevisionViewController: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+            let currentText = answerTextView.text ?? ""
+            guard let stringRange = Range(range, in: currentText) else { return false }
+    
+            let changedText = currentText.replacingCharacters(in: stringRange, with: text)
+    
+        
+            return changedText.count <= 200
+        
+        }
+    func textViewDidChange(_ textView: UITextView) {
+        wordCounterLabel.text = "\(textView.text.count)/200"
+    }
+}
--- FamilyDiary/FamilyDiary/ViewController/QuestionViewController.swift
+++ FamilyDiary/FamilyDiary/ViewController/QuestionViewController.swift
@@ -11,14 +11,13 @@ import UIKit
 class QuestionViewController: UIViewController {
 
     @IBOutlet weak var questionDescription: UILabel!
-    
     @IBOutlet weak var answerTextView: UITextView!
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var answeredUsers: UILabel!
     @IBOutlet weak var wordCountLabel: UILabel!
     
     var row: Int?
-    var question: Question?
+    var question: Question? 
     var answers: [Answer]?
     var answerInput: String?
     var answeredUsersLabel: String = ""
@@ -36,17 +35,15 @@ class QuestionViewController: UIViewController {
         saveButton.layer.borderWidth = 2.0
         answerTextView.layer.borderWidth = 0.5
         answerTextView.layer.cornerRadius = 10
-
+        answerTextView.textContainerInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
     }
     
     // 답변 저장 버튼 클릭 이벤트
     @IBAction func saveAnswer(_ sender: Any) {
         answerList[row!].append(Answer(answeredUser: currentUser, answerDescription: answerTextView.text))
         print(answerList)
-        
-//        answer = answerTextView.text
-//        question?.answers.append(Answer(answeredUser: currentUser, answerDescription: answerTextView.text)) // 실제로는 로그인 된 유저의 아이디를 넣어야 함
-//        print(question)
+        navigationController?.popViewController(animated: true)
+        dismiss(animated: true, completion: nil)
     }
     
     
@@ -61,7 +58,17 @@ class QuestionViewController: UIViewController {
             answeredUsers.text = "답변한 유저: \(answeredUsersName)"
         }
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? AnswerTableViewController{
+            destination.answers = answers
+            destination.question = question
+            destination.row = row
+        }
+    }
 }
+
+
 
 
 
@@ -79,6 +86,4 @@ extension QuestionViewController: UITextViewDelegate {
     func textViewDidChange(_ textView: UITextView) {
         wordCountLabel.text = "\(textView.text.count)/200"
     }
-    
-    
 }
